3a4338297e66032aa9a89487ff2eba4f4f13e423
Lucas Debatin <lucasdebatin@univali.br>
2021-11-17 20:30:07 -0300

    voltado para as configurações locais do servidor de banco de dados

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     // Fazer a conexão com o BD --> ip do servidor, nome do usuário, senha do usuário, nome do BD
-    $mysqli = new mysqli('50.116.112.119', 'dlweb958_aulabd', 'UTHs0fKZ3B', 'dlweb958_biblioteca_univali');
+    $mysqli = new mysqli('localhost', 'root', 'master', 'biblioteca_univali');
 
     if (mysqli_connect_error()) {
         echo "Erro ao conectar com o BD: " . mysqli_connect_error();
